d339316ff1132d3b4030ce819ab16e620c7b8922
ssnyder555 <ssnyder555@live.com>
2018-12-05 13:04:23 -0600

    show page and index page working

--- controllers/fruits.js
+++ controllers/fruits.js
@@ -1,19 +1,109 @@
 const express = require('express');
 const router  = express.Router();
 
-// require the models that our controllers needs
+// Require the models that our controller needs
 const Fruits = require('../models/fruits');
 
-// Index Route
+// Index route
 // Shows all the fruits
 router.get('/', (req, res) => {
-  res.render('index.ejs', {fruits: Fruits});
+
+  Fruits.find({}, (err, allFruits) => {
+    if(err){
+      console.log(err);
+    } else {
+      console.log(allFruits)
+      // allFruits, is still an array
+      res.render('index.ejs', {fruits: allFruits});
+    }
+  });
+
 });
+
 
 router.get('/new', (req, res) => {
   res.render('new.ejs');
 });
 
+
 router.post('/', (req, res) => {
-  console.log(req.body, ' this is where info from the schema');
+  console.log(req.body, ' this is where our info from the fruit form will live');
+
+  if(req.body.readyToEat === 'on'){
+    req.body.readyToEat = true;
+  } else {
+    req.body.readyToEat = false;
+  }
+  // Rewrite this code to use mongodb
+  Fruits.create(req.body, (err, createdFruit) => {
+    if(err){
+      console.log(err)
+    } else {
+      console.log(createdFruit);
+      res.redirect('/fruits')
+    }
+  })
+
 });
+
+
+router.get('/:id/edit', (req, res) => {
+
+  Fruits.findById(req.params.id, (err, foundFruit) => {
+      res.render('edit.ejs', {
+        fruit: foundFruit,
+
+      });
+  });
+
+});
+
+
+
+// url params, is extra stuff we can
+// put in our url for our server to dynamically
+// read
+
+
+
+// url params - is a variable that we
+// can capture in the url
+
+// Show route
+// Shows a single fruit
+router.get('/:id', (req, res) => {
+  console.log(req.params);
+  Fruits.findById(req.params.id, (err, foundFruit) => {
+    console.log(foundFruit, ' foundFruit')
+      res.render('show.ejs', {
+        fruit: foundFruit
+      });
+  });
+});
+
+
+router.delete('/:id', (req, res) => {
+  console.log(req.params.id, ' id in delete route');
+  Fruits.findByIdAndRemove(req.params.id, (err, deleteFruit) => {
+    res.redirect('/fruits');
+  });
+});
+
+
+router.put('/:id', (req, res) => {
+  console.log(req.params.id, ' id in the put route');
+  console.log(req.body, ' this should be our form data');
+  if(req.body.readyToEat === 'on'){
+    req.body.readyToEat = true;
+  } else {
+    req.body.readyToEat = false;
+  }
+
+  Fruits.findByIdAndUpdate(req.params.id, req.body, (err, updatedModel) => {
+    res.redirect('/fruits')
+  });
+
+
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,46 +1,55 @@
-const express          = require('express');
-const app              = express();
-const bodyParser       = require('body-parser');
-const methodOverride   = require('method-override');
+const express = require('express');
+const app     = express();
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 // require our database
 require('./db/db');
-// require our controller
+
+// Require our controller
 const fruitsController = require('./controllers/fruits');
 
 
 
-// setting up the middle ware
-// middleware our function that happens sycronously
-// in the request from the client
+// Setting up middleWare
+// Middleware our functions that happen sychronously
+// in the request from the client on the server
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(methodOverride('_method'));
 
+// Your controllers should always  be last in your
+// middleware stack
+// /fruits here, sets up every route in the fruitscontroller
+// aka the whole router object we exporting to the route /fruits
+app.use('/fruits', fruitsController);
+
+// MVC - Architecture Pattern
+// Model View Controller
+
+// Model is a representation of our
+// data
+
+// The view - What that data looks like
+// In essence its what we are sending
+// to the client
+
+// Controller is a way to organize
+// modularize our code, the glue
+// between the model and the view
+// It will look like our routes
 
 
-// app.use('fruits', fruitsController)
 
-app.get('/', (req, res) =>{
-  res.send('this is the index page')
+app.get('/', (req, res) => {
+  res.send('This is fruits app')
 });
 
-// this will be our data aka our model
-const fruits = ['apple', 'banna', 'pineapple'];
 
 
-app.get('/fruits', (req, res) =>{
-  res.send(fruits);
-})
 
-// query params is extra stuff we can put in our url for
-// our server to  dynamicaly read
 
-// query param - is a variable that we can capture in the url
-app.get('/fruits/:index', (req, res) => {
-  console.log(req.params);
-  res.send(fruits[req.params.index]);
-})
+
 
 
 app.listen(3000, () => {
-  console.log('we are listening Stephen');
-});
+  console.log('listening on port 3000')
+})
